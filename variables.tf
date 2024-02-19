69b00834ec0049bca859729c4a3d2fad30c0b348
iamMuruganRaja <124397792+iamMuruganRaja@users.noreply.github.com>
2024-02-19 14:06:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_access_key" {
-  description = "AKIATOJD6L274KTY2N5D"
+  description =
 }
 
 variable "aws_secret_key" {
-  description = "uTWr3GWeENZtyE7Me2LGLi2GRyBVO+rk+tghanxt"
+  description = 
 }
